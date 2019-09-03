567197c481d0efeece8c6d84bb853db0766f45c4
HR <h@rehman.email>
2019-09-03 13:03:04 +0100

    implement entire repo cloning

--- ghclone.py
+++ ghclone.py
@@ -29,17 +29,20 @@ import errno
 from sys import exit
 from docopt import docopt
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 GH_API_BASE_URL = 'https://api.github.com'
+GH_REPO_ENDPOINT = GH_API_BASE_URL + '/repos/{}/{}'
 GH_REPO_CONTENTS_ENDPOINT = GH_API_BASE_URL + '/repos/{}/{}/contents'
 BASE_NORMALIZE_REGEX = re.compile(r'.*github\.com\/')
 
 req = requests.Session()
-req.headers.update({'User-Agent': 'git.io/ghclone '+__version__})
+req.headers.update({'User-Agent': 'git.io/ghclone ' + __version__})
+
 
 def exit_with_m(m='An error occured'):
     print(m)
     exit(1)
+
 
 def mkdir_p(path):
     try:
@@ -50,6 +53,7 @@ def mkdir_p(path):
         else:
             raise
 
+
 def clone_file(download_url, file_path):
     """
     Clones the file at the download_url to the file_path
@@ -58,38 +62,41 @@ def clone_file(download_url, file_path):
     try:
         r.raise_for_status()
     except Exception as e:
-        exit_with_m('Failed cloning ' + download_url, e)
+        exit_with_m('Failed to clone ' + download_url, e)
 
     with open(file_path, 'wb') as fd:
         for chunk in r.iter_content(chunk_size=128):
             fd.write(chunk)
 
+
 def clone(base_url, path=None, ref=None):
     """
     Recursively clones the path
     """
-    req_url = base_url if not path else os.path.join(base_url, path)
+    if path:
+        req_url = os.path.join(base_url, path)
+        # Create path locally
+        mkdir_p(path)
+    else:
+        req_url = base_url
+
     # Get path metadata
     r = req.get(req_url) if not ref else req.get(req_url, params={'ref': ref})
     try:
         r.raise_for_status()
     except Exception as e:
-        exit_with_m('Failed fetching metadata for ' + path, e)
+        exit_with_m('Failed to fetch metadata for ' + path, e)
     repo_data = r.json()
 
-    # Create path locally
-    mkdir_p(path)
-
-    if isinstance(repo_data, list):
-        # Recursively clone content
-        for item in repo_data:
-            if item['type'] == 'dir':
-                # Fetch dir recursively
-                clone(base_url, item['path'], ref)
-            else:
-                # Fetch the file
-                clone_file(item['download_url'], item['path'])
-                print('Cloned', item['path'])
+    # Recursively clone content
+    for item in repo_data:
+        if item['type'] == 'dir':
+            # Fetch dir recursively
+            clone(base_url, item['path'], ref)
+        else:
+            # Fetch the file
+            clone_file(item['download_url'], item['path'])
+            # print('Cloned', item['path'])
 
 
 ###
@@ -105,17 +112,31 @@ def main():
     gh_url = arguments['<url>']
     token = arguments['--token']
     if token:
-        req.headers.update({'Authorization': 'token '+token[0]})
+        req.headers.update({'Authorization': 'token ' + token[0]})
     # Normalize & parse input
-    normal_gh_url = re.sub(BASE_NORMALIZE_REGEX, '', gh_url).replace('/tree', '')
-    gh_url_comps = normal_gh_url.split('/')
-    user, repo = gh_url_comps[:2]
-    ref = gh_url_comps[2]
-    path = os.path.join(*gh_url_comps[3:])
+    normal_gh_url = re.sub(BASE_NORMALIZE_REGEX, '',
+                           gh_url).replace('/tree', '')
+    gh_args = normal_gh_url.split('/')
+    user, repo = gh_args[:2]
+
+    if len(gh_args) > 2:
+        # Clone subdirectory
+        ref = gh_args[2]
+        path = os.path.join(*gh_args[3:])
+        print("Cloning into '%s'..." % path)
+    else:
+        # Clone entire repo
+        mkdir_p(repo)
+        os.chdir(repo)
+        ref = None
+        path = None
+        print("Cloning into '%s'..." % repo)
+
     api_req_url = GH_REPO_CONTENTS_ENDPOINT.format(user, repo)
-    print("Cloning into '%s'..." % path)
+
     clone(api_req_url, path, ref)
     print("done.")
+
 
 if __name__ == '__main__':
     main()
